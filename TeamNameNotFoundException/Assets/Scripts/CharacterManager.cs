b326ef4f375d0c61108c1380b9d6113a5e8320ef
Juan Naputi <jnaputi253@gmail.com>
2016-05-22 21:45:34 -0700

    Added basic character selection screen

---
+++ TeamNameNotFoundException/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterManager : MonoBehaviour {
+
+	public void CharacterSelected(int id) {
+		Debug.Log ("Character selected: " + CharacterName(id));
+		Debug.Log ("Number selected: " + id);
+	}
+
+	string CharacterName(int id) {
+		switch(id) {
+		case 0:
+			return "Ashley";
+
+		case 1:
+			return "Juan";
+
+		case 2:
+			return "Stormi";
+
+		case 3:
+			return "Andrew";
+
+		default:
+			return "Who is that?";
+		}
+	}
+}
