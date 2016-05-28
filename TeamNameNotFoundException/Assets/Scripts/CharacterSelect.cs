2831a28593e94833b8ab3cf3baf584a0faf98b89
ashleycorinne <amcbride@eagles.ewu.edu>
2016-05-28 16:29:35 -0700

    sound effect files

---
+++ TeamNameNotFoundException/Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setCharacter(string name)
+    {
+        ApplicationData.characterName = name;
+    }
+}
